bc0957af0643607400df0e4b59f9f822bca1081e
ZimJi <hieuboyfc@gmail.com>
2024-08-20 13:14:04 +0700

    [Commit Code] - Date: 20/08/2024 13:14

--- api-gateway/src/main/java/com/zimji/gateway/api/FallbackAPI.java
+++ api-gateway/src/main/java/com/zimji/gateway/api/FallbackAPI.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/fallback1")
+@RequestMapping("/fallback")
 public class FallbackAPI {
 
     @GetMapping
